869f2794aaca573ff18b4ba5284b9506bde15994
Eric Gomez <eric.gomez.sanchez@gmail.com>
2022-12-03 14:53:57 -0600

    Finalizar vista Live mostrando las salas disponibles con la posibilidad de dar de alta nuevas salas

--- src/views/Rooms.vue
+++ src/views/Rooms.vue
@@ -1,11 +1,45 @@
 <template>
-  <div>
-    rooms
-  </div>
+  <v-container fluid>
+    <new-room-modal :dialog="dialog" @close="dialog = false" @submit="createNewRoom" />
+    <fab-button color="warning" @clicked="dialog = true" />
+
+    <v-card class="mx-auto" tile v-if="$store.state.rooms.rooms.length">
+      <room-list-item
+        v-for="room in $store.state.rooms.rooms"
+        :key="room._id"
+        :room="room"
+        @goRoom="goRoom(room)"
+      />
+    </v-card>
+    
+    <v-alert v-else dark color="red" class="title">No hay salas disponibles, ¡crea una!</v-alert>
+  </v-container>
 </template>
 
 <script>
+  import NewRoomModal from "../components/rooms/NewRoomModal";
+  import FabButton from "../components/ui/FabButton";
+  import RoomListItem from "../components/rooms/RoomListItem";
+
   export default {
-    name: "Rooms"
+    name: "Rooms",
+    components: { RoomListItem, FabButton, NewRoomModal },
+    mounted() {
+      this.$socket.emit('rooms/all');
+    },
+    data() {
+      return {
+        dialog: false
+      }
+    },
+    methods: {
+      async createNewRoom(data) {
+        await this.$socket.emit('rooms/create', data);
+      },
+      goRoom(room) {
+        this.$store.commit("rooms/setRoom", room, { root: true });
+        this.$router.push({ name: "Room", params: { id: room._id }});
+      }
+    }
   }
 </script>
