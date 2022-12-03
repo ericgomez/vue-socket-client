73913a31d7e605014e1ab0551ea95f0e3d93bc0b
Eric Gomez <eric.gomez.sanchez@gmail.com>
2022-12-02 18:46:20 -0600

    Main.js y definir un EventBus global

--- src/main.js
+++ src/main.js
@@ -1,14 +1,19 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
-import vuetify from './plugins/vuetify'
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router';
+import store from './store';
+import vuetify from './plugins/vuetify';
+import "./plugins/vue-resource";
+import "./plugins/vuelidate";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+
+// Create a EventBus Global
+Vue.prototype.$rootBus = new Vue;
 
 new Vue({
   router,
   store,
   vuetify,
   render: h => h(App)
-}).$mount('#app')
+}).$mount('#app');
