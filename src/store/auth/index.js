02de76a50328dca519e1f4ae3b90b1d20627a3a0
Eric Gomez <eric.gomez.sanchez@gmail.com>
2022-12-02 18:14:15 -0600

    Módulo de autenticación con Vuex

---
+++ src/store/auth/index.js
@@ -0,0 +1,79 @@
+import Vue from "vue";
+
+const authModule = {
+  namespaced: true,
+  state: {
+    token: null
+  },
+  actions: {
+    login: async function ({ commit }, { username, password }) {
+      try {
+        commit('setProcessing', { show: true, message: null }, { root: true }); // this mutation is in the root
+        const { body } = await Vue.http.post('login', { username, password });
+        commit('auth/setUser', body, { root: true });
+        commit('setNotification', {
+          show: true,
+          message: "Bienvenid@!",
+          color: "primary"
+        },
+          { root: true }); // this mutation is in the root
+
+        await Vue.prototype.$rootBus.$emit("routerPush", { name: "Rooms", params: {} });
+      } catch (e) {
+        commit('setNotification', {
+          show: true,
+          message: e.body.message,
+          color: "red"
+        },
+          { root: true }); // this mutation is in the root
+      } finally {
+        commit('setProcessing', { show: false, message: null }, { root: true }); // this mutation is in the root
+      }
+    },
+    register: async function ({ commit }, { username, password }) {
+      try {
+        commit('setProcessing', { show: true, message: null }, { root: true }); // this mutation is in the root
+        const { body } = await Vue.http.post('register', { username, password });
+        commit('auth/setUser', body, { root: true });
+      } catch (e) {
+        commit('setNotification', {
+          show: true,
+          message: e.body.message,
+          color: "red"
+        },
+          { root: true }); // this mutation is in the root
+      } finally {
+        commit('setProcessing', { show: false, message: null }, { root: true });
+      }
+    },
+    logout: async ({ commit }) => {
+      await commit('auth/logout', null, { root: true });
+
+      await Vue.prototype.$rootBus.$emit("routerPush", { name: "Login", params: {} });
+    }
+  },
+  mutations: {
+    setUser(state, data) {
+      state.token = data.token;
+    },
+    logout(state) {
+      state.token = null;
+    }
+  },
+  getters: {
+    user(state) {
+      // check if exist token
+      if (!state.token) return null;
+
+      const base64Url = state.token.split('.')[1];
+      const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+      const jsonPayload = decodeURIComponent(atob(base64).split('').map(function (c) {
+        return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+      }).join(''));
+
+      return JSON.parse(jsonPayload);
+    }
+  }
+};
+
+export default authModule;
