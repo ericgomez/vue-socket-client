92001d0a7f1574eba0e2b8c3e65fa78f8b94b583
Eric Gomez <eric.gomez.sanchez@gmail.com>
2022-12-02 18:59:55 -0600

    Preparar el archivo App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,55 +1,62 @@
 <template>
   <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
+    <v-app-bar app class="purple darken-2" dark v-if="$store.state.auth.token">
       <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
+        <span class="display-1">Chat APP</span>
       </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
+      <v-spacer />
+      <span class="title secondary pa-3">Hola {{ $store.getters['auth/user'].username }}</span>
+      <span class="title pa-3 warning font-weight-bold ml-4">{{ totalSockets }}</span>
+      <v-btn right text @click="exit">Salir</v-btn>
     </v-app-bar>
 
-    <v-main>
-      <router-view/>
-    </v-main>
+    <v-content class="blue-grey darken-4">
+
+      <snack-bar />
+    
+      <private-snack-bar />
+      <div id="app">
+        <router-view />
+        <progress-bar-circular
+          v-if="$store.state.processing.show"
+          :processing="$store.state.processing"
+        />
+    
+      </div>
+    </v-content>
   </v-app>
 </template>
 
 <script>
+  import { mapActions } from "vuex";
+  import SnackBar from "./components/ui/SnackBar";
+  import ProgressBarCircular from "./components/ui/ProgressBarCircular";
+  import PrivateSnackBar from "./components/ui/PrivateSnackBar";
 
-export default {
-  name: 'App',
-
-  data: () => ({
-    //
-  }),
-};
+  export default {
+    name: "App",
+    components: { PrivateSnackBar, ProgressBarCircular, SnackBar },
+    async mounted() {
+      // listening EventBus Global
+      this.$rootBus.$on("routerPush", route => {
+        this.$router.push(route);
+      })
+    },
+    methods: {
+      ...mapActions({
+        logout: "auth/logout"
+      }),
+      exit() {
+        this.$socket.emit('logout', this.$store.getters['auth/user'].username);
+        this.$socket.disconnect();
+        this.logout();
+      }
+    },
+    computed: {
+      totalSockets() {
+        const count = this.$store.state.live.countLiveUsers;
+        return count > 1 ? `${count} usuarios conectados!` : `${count} usuario conectado!`;
+      }
+    }
+  };
 </script>
